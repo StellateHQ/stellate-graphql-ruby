458a04e10f2b186cf4b7ab4020c8b89466d6b4f1
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2024-03-13 11:47:22 +0100

    bump to 0.0.2

--- lib/stellate.rb
+++ lib/stellate.rb
@@ -4,7 +4,7 @@ require 'uri'
 require 'net/http'
 
 module Stellate
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 
   # Extend your GraphQL::Schema with this module to enable easy Stellate
   # Metrics Logging.
